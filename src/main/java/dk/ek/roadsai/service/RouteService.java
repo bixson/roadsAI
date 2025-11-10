f871d7fbad7bd44746487a0655ec1eb9ac805c97
bixson <alexanderornb@gmail.com>
2025-11-10 13:19:39 +0100

    Added more precice polyline for coordinates in RouteService.java

--- src/main/java/dk/ek/roadsai/service/RouteService.java
+++ src/main/java/dk/ek/roadsai/service/RouteService.java
@@ -9,17 +9,19 @@ import java.util.Map;
 @Service
 public class RouteService {
 
-    // TODO: Replace with final polyline when ready.
     private static final List<List<Double>> RVK_ISF = List.of(
-        List.of(-21.9082, 64.1289), // Reykjavík, Faxaflói
-        List.of(-21.9603, 64.4755), // Hafnarfjall (Vegagerðin)
-        List.of(-21.9105, 64.5609), // Borgarnes (approx)
-        List.of(-21.5154, 64.8716), // Brattabrekka (Vegagerðin)
-        List.of(-21.8330, 65.5524), // Þröskuldar (Vegagerðin)
-        List.of(-21.6813, 65.6873), // Hólmavík (approx)
-        List.of(-22.1291, 65.7503), // Steingrímsfjarðarheiði (Vegagerðin)
-        List.of(-22.6817, 66.0449), // Ögur (Vegagerðin)
-        List.of(-23.1699, 66.0596)  // Arnarfjörður / Ísafjörður (approx)
+        List.of(-21.8046, 64.1238), // Reykjavík, Orkan Vesturlandsvegi
+        List.of(-21.9603, 64.4755), // Hafnarfjall, nálægt vindskilti
+        List.of(-21.9101, 64.5439), // Borgarnes, N1 bensínstöð
+        List.of(-21.5154, 64.8716), // Brattabrekka, Dalabyggð
+        List.of(-21.7632, 65.1082), // Búðardalur (gamla Kaupfélagið)
+        List.of(-21.8330, 65.5524), // Þröskuldar
+        List.of(-21.6951, 65.7015), // Hólmavík, við afleggjara/flugvöll
+        List.of(-22.1291, 65.7503), // Steingrímsfjarðarheiði
+        List.of(-22.7303, 66.0403), // Ögur
+        List.of(-22.9888, 66.0279), // Súðavík
+        List.of(-23.0465, 66.0977),  // Arnarfjörður, Ísafjörður
+        List.of(-23.1239, 66.0746)   // Ísafjörður, N1 bensínstöð
     );
 
     public List<List<Double>> getCoordinates() {
