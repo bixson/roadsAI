bc6ed42de29c4eb4fc160c2c03b9e560cde9c9e8
bixson <alexanderornb@gmail.com>
2025-11-13 13:16:47 +0100

    Fixed wrong comment

--- src/main/java/dk/ek/roadsai/dto/vegagerdin/VegagerdinArrayDto.java
+++ src/main/java/dk/ek/roadsai/dto/vegagerdin/VegagerdinArrayDto.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
 import java.util.List;
 
-///  Vegargerðin returns JSON/XML in API
+/// Vegagerðin returns JSON array in API
 /// wraps @VegagerdinItemDto into an array
 public class VegagerdinArrayDto {
 
